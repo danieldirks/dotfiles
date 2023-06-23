c0b7d8384126f8d72921b5191b05b502b1709e65
Daniel Dirks <dandirks@web.de>
2023-06-24 01:53:43 +0200

    Add chezmoi aliases

--- home/private_dot_config/zsh/aliases.zsh
+++ home/private_dot_config/zsh/aliases.zsh
@@ -4,5 +4,12 @@ alias ls='ls --color=auto'
 alias ll='ls -Alh --color=always'
 alias nv='nvim'
 
+# defaults
 alias vim='nvim'
 alias cat='bat --style=plain'
+
+# chezmoi
+alias ca='chezmoi apply'
+alias ccd='chezmoi cd'
+alias ce='chezmoi edit'
+alias cu='chezmoi update'
