c806f4a8fee82add2de8f824662257406606d162
Daniel Dirks <dandirks@web.de>
2023-06-23 23:56:09 +0200

    Add zsh autocompletion with fzf

--- home/private_dot_config/fzf/fzf.sh
+++ home/private_dot_config/fzf/fzf.sh
@@ -1,13 +1,24 @@
+export FZF_BASE='/usr/bin/fzf'
+
 export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git'
-export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:#eceff4,bg:#2e3440,hl:#5e81ac --color=fg+:#88c0d0,bg+:#2e3440,hl+:#5e81ac --color=info:#b48ead,prompt:#5e81ac,pointer:#88c0d0 --color=marker:#a3be8c,spinner:#b48ead,header:#81a1c1 --bind ctrl-j:down,ctrl-k:up'
-export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --bind ctrl-j:down,ctrl-k:up'
-export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --scroll-off=5'
-export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --border=none'
-export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --prompt="» " --pointer="›" --marker="›"'
-export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --preview-window=right,border-left --preview "bat --color=always --style=numbers --line-range=:500 {}"'
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS"-m\
+    --color=fg:-1,bg:-1,hl:6 \
+    --color=fg+:-1:regular,bg+:0,hl+:-1:regular \
+    --color=gutter:-1 \
+    --color=info:14,prompt:6,pointer:6 \
+    --color=marker:4,spinner:14,header:4 \
+    --prompt=\"» \" --pointer=\"›\" --marker=\"›\" \
+    --bind ctrl-j:down,ctrl-k:up \
+    --scroll-off=5 \
+    --border=none \
+    --preview-window=right,border-left --preview \"bat --color=always --style=numbers --line-range=:500 {}\""
+
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="$FZF_DEFAULT_OPTS"
+
 export FZF_CTRL_R_OPTS="$FZF_DEFAULT_OPTS --preview-window=hidden"
+
 export FZF_ALT_C_COMMAND="fd --type directory --hidden --exclude .git"
 export FZF_ALT_C_OPTS="$FZF_DEFAULT_OPTS --preview-window=hidden"
+
 export FZF_TMUX=1
---
+++ home/private_dot_config/zsh/fzf-tab.zsh
@@ -0,0 +1,48 @@
+zstyle ':fzf-tab:*' prefix ''  # hide colorized prefix
+zstyle ':fzf-tab:*' default-color $'\033[37m'  # default color white
+
+# groups
+zstyle ':fzf-tab:*' show-group none  # hide groups
+#zstyle ':fzf-tab:*' switch-group ',' '.'  # switch types with . & ,
+zstyle ':completion:*:descriptions' format '[%d]'  # style of group names
+FZF_TAB_GROUP_COLORS=(
+    $'\033[34m' $'\033[33m' $'\033[32m' $'\033[31m' $'\033[35m' \
+    $'\033[34m' $'\033[33m' $'\033[32m' $'\033[31m' $'\033[35m' \
+    $'\033[34m' $'\033[33m' $'\033[32m' $'\033[31m' $'\033[35m' \
+    $'\033[34m' $'\033[33m' $'\033[32m' $'\033[31m' $'\033[35m'
+)
+zstyle ':fzf-tab:*' group-colors $FZF_TAB_GROUP_COLORS  # colors for groups
+
+# controls
+zstyle ':fzf-tab:*' fzf-bindings 'space:accept'  # accept on space
+zstyle ':fzf-tab:*' accept-line enter  # directly run on enter
+
+
+
+###################
+### integration ###
+###################
+
+# disable previews for commands and arguments
+zstyle ':fzf-tab:complete:*:options' fzf-preview
+zstyle ':fzf-tab:complete:*:argument-1' fzf-preview
+
+# cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'fd -d1 --hidden . $realpath'  # cd preview
+
+# variables
+zstyle ':fzf-tab:complete:(-command-|-parameter-|-brace-parameter-|export|unset|expand):*' fzf-preview 'echo ${(P)word}'
+
+# git
+zstyle ':completion:*:git-checkout:*' sort false  # ignore order on git checkout
+zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview 'git diff $word | delta'
+zstyle ':fzf-tab:complete:git-log:*' fzf-preview 'git log --color=always $word'
+zstyle ':fzf-tab:complete:git-help:*' fzf-preview 'git help $word | bat -plman --color=always'
+zstyle ':fzf-tab:complete:git-show:*' fzf-preview 'case "$group" in	"commit tag") git show --color=always $word ;; *) git show --color=always $word | delta ;; esac'
+zstyle ':fzf-tab:complete:git-checkout:*' fzf-preview 'case "$group" in "modified file") git diff $word | delta ;; "recent commit object name") git show --color=always $word | delta ;; *) git log --color=always $word ;; esac'
+
+# systemd
+zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
+
+# tmux
+#zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup  # activate tmux popup
