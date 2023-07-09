289a79ade17c2f8293c0368bbcad9cb3a10fc59d
Daniel Dirks <dandirks@web.de>
2023-07-09 18:23:44 +0200

    Add profiles to hyper config on windows

--- home/AppData/Roaming/Hyper/dot_hyper.js
+++ home/AppData/Roaming/Hyper/dot_hyper.js
@@ -7,16 +7,29 @@ module.exports = {
         //shell: 'wsl.exe',
         //shellArgs: ['--cd', '~'],
 
-        // hyper-lanuch-menu
-        shells: [
-            {name: "WSL", shell: "wsl.exe", args: ["--cd", "~"], shortcut: "L", default: true},
-            {name: "PowerShell", shell: "powershell.exe", shortcut: "P"},
-            {name: "PowerShell (Admin)", shell: "powershell.exe", args: ["Start-Process", "powershell", "-Verb", "runAs"], shortcut: "O"},
+        defaultProfile: "Ubuntu (WSL)",
+        profiles: [
+            {
+                name: "Ubuntu (WSL)",
+                config: {
+                    shell: "wsl.exe",
+                    shellArgs: ["--cd", "~"],
+                },
+            },
+            {
+                name: "PowerShell",
+                config: {
+                    shell: "powershell.exe",
+                },
+            },
+            {
+                name: "PowerShell (Admin)",
+                config: {
+                    shell: "powershell.exe",
+                    shellArgs: ["Start-Process", "powershell", "-Verb", "runAs"],
+                },
+            },
         ],
-        showShellNotifications: false,
-        openOnSelect: true,
-        setOnSelect: false,
-        selectShellKeymap: "ctrl+alt",
     },
-    plugins: ["nord-hyper", "hyper-launch-menu"],
+    plugins: ["nord-hyper"],
 };
