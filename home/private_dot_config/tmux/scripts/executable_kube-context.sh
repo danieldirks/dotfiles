764e9d489b50435cb1337663df11cb2b9af442ea
Daniel Dirks <dandirks@web.de>
2023-07-18 19:40:01 +0200

    Add tmux kube context status bar block

--- home/private_dot_config/tmux/scripts/executable_kube-context.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if command -v kubectl &> /dev/null ; then
-    CONTEXT=$(kubectl config view --minify -o jsonpath='{.current-context}' 2> /dev/null)
-    if [[ -n $CONTEXT ]]; then
-        echo " k8s:$CONTEXT "
-    fi
-fi
