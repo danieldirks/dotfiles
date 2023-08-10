94c390adcce880d2c643635d77cb90404df33a60
Daniel Dirks <dandirks@web.de>
2023-08-10 12:51:34 +0200

    Fix templating issues on windows

--- home/.chezmoiscripts/run_onchange_before_activate-wsl.ps1
+++
@@ -1,9 +0,0 @@
-{{/* activates wsl on windows. */}}
-{{- if eq .os.chezmoi "windows" -}}
-Write-Host "Activating wsl..."
-
-if (!(Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux).State) {
-    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-}
-
-{{- end -}}
