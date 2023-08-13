757390ef423d0537d9e256e92fd8be8884774395
Daniel Dirks <dandirks@web.de>
2023-08-13 22:44:33 +0200

    Add nvim function mappings

--- home/private_dot_config/nvim/lua/mappings.lua
+++ home/private_dot_config/nvim/lua/mappings.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ";"
-vim.keymap.set("n", "<F2>", ":UndoTreeToggle<CR>")
+
+vim.keymap.set("n", "<F3>", ":UndoTreeToggle<CR>")
+vim.keymap.set("n", "<F2>", ":Neotree<CR>")
+vim.keymap.set("n", "<F5>", ":Neorg index<CR>")
