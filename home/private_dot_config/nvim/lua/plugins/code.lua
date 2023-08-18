e055a481acd666478502e2a6aae1e86d353babe6
Daniel Dirks <dandirks@web.de>
2023-08-18 22:56:50 +0200

    Add nvim completions and snippets

--- home/private_dot_config/nvim/lua/plugins/code.lua
+++ home/private_dot_config/nvim/lua/plugins/code.lua
@@ -1,4 +1,5 @@
 return {
+    -- language support
     {
         -- nvim-lspconfig https://github.com/neovim/nvim-lspconfig
         "neovim/nvim-lspconfig",
@@ -62,6 +63,7 @@ return {
         },
     },
 
+    -- syntax
     {
         -- nvim-treesitter https://github.com/nvim-treesitter/nvim-treesitter
         "nvim-treesitter/nvim-treesitter",
@@ -73,5 +75,67 @@ return {
         config = function(_, opts)
             require('nvim-treesitter.configs').setup(opts)
         end
+    },
+
+    -- autocompletion
+    {
+        -- nvim-cmp https://github.com/hrsh7th/nvim-cmp
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-nvim-lua",
+            "saadparwaiz1/cmp_luasnip",
+        },
+        config = function()
+            local cmp = require("cmp")
+            vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "nvim_lua" },
+                    { name = "luasnip" },
+                }, {
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+            })
+
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "path" },
+                }, {
+                    { name = "cmdline" },
+                }),
+            })
+        end
+    },
+
+    -- snippets
+    {
+        -- LuaSnip https://github.com/L3MON4D3/LuaSnip
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+            "rafamadriz/friendly-snippets"  -- collection of community snippets
+        },
+        init = function()
+            require("luasnip.loaders.from_snipmate").lazy_load()
+        end,
     },
 }
---
+++ home/private_dot_config/nvim/snippets/README.md
@@ -0,0 +1,3 @@
+# Snippets
+
+See [honza/vim-snippets](https://github.com/honza/vim-snippets/tree/master/snippets) for examples.
