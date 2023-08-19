1d675fe5a4af617553fdc54400b672af56c95d28
Daniel Dirks <dandirks@web.de>
2023-08-19 16:25:12 +0200

    Add nvim dashboard

--- home/private_dot_config/nvim/lua/plugins/ui.lua
+++ home/private_dot_config/nvim/lua/plugins/ui.lua
@@ -15,6 +15,7 @@ return {
             transparent_background = false,
             term_colors = true,
             integrations = {
+                alpha = true,
                 gitsigns = true,
                 mason = true,
                 neotree = true,
@@ -156,6 +157,52 @@ return {
     },
 
     {
+        -- alpha-nvim https://github.com/goolord/alpha-nvim
+        -- dashboard
+        "goolord/alpha-nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+            { "MaximilianLloyd/ascii.nvim", dependencies = "MunifTanjim/nui.nvim" },
+        },
+        event = "VimEnter",
+        config = function()
+            local dashboard = require "alpha.themes.dashboard"
+
+            -- art
+            dashboard.section.header.val = require("ascii").get_random("text", "neovim")
+
+            -- buttons
+            dashboard.section.buttons.val = {
+                dashboard.button("f", "  Find File", ":Telescope find_files<CR>"),
+                dashboard.button("n", "  New File", ":ene <BAR> startinsert <CR>"),
+                dashboard.button("r", "󱑁  Recent Files", ":Telescope oldfiles<CR>"),
+                dashboard.button("u", "  Update Plugins", ":Lazy update<CR>"),
+                dashboard.button("q", "󰗼  Quit", ":qa!<CR>"),
+            }
+
+            -- footer
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "LazyVimStarted",
+                callback = function()
+                    local version = vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch
+                    local stats = require("lazy").stats()
+                    local count = stats.count
+                    local time = (math.floor(stats.startuptime + 0.5))
+                    dashboard.section.footer.val = "Neovim " .. version .. " loaded with " .. count .. " plugins in " .. time .. "ms"
+                    pcall(vim.cmd.AlphaRedraw)
+                end,
+            })
+
+            dashboard.section.footer.opts.hl = "AlphaFooter"
+            dashboard.section.header.opts.hl = "AlphaHeader"
+            dashboard.section.buttons.opts.hl = "AlphaButton"
+
+            dashboard.opts.opts.noautocmd = false
+            require("alpha").setup(dashboard.config)
+        end
+    },
+
+    {
         -- which-key https://github.com/folke/which-key.nvim
         -- key mappings help popup
         "folke/which-key.nvim",
@@ -167,7 +214,7 @@ return {
     },
 
     {
-        -- indent-blanckline.nvim https://github.com/lukas-reineke/indent-blankline.nvim
+        -- indent-blankline.nvim https://github.com/lukas-reineke/indent-blankline.nvim
         "lukas-reineke/indent-blankline.nvim",
         opts = {
             show_current_context = true,
