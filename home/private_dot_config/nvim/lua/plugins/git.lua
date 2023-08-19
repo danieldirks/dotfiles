d8c21f21e6b65b93388e11ead230684117d3018c
Daniel Dirks <dandirks@web.de>
2023-08-19 13:04:44 +0200

    Update nvim git plugins

--- home/private_dot_config/nvim/lua/plugins/git.lua
+++ home/private_dot_config/nvim/lua/plugins/git.lua
@@ -3,6 +3,22 @@ return {
         -- gitsigns.nvim https://github.com/lewis6991/gitsigns.nvim
         -- git gutter signs
         "lewis6991/gitsigns.nvim",
-        opts = {},
+        config = true
+    },
+
+    {
+        -- neogit https://github.com/NeogitOrg/neogit
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+            "sindrets/diffview.nvim",
+        },
+        config = true
+    },
+
+    {
+        -- diffview.nvim https://github.com/sindrets/diffview.nvim
+        "sindrets/diffview.nvim",
     },
 }
