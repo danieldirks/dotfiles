5014f782ad5298e928bf10f4c5fdb870fbed4138
Daniel Dirks <dandirks@web.de>
2023-08-20 11:12:43 +0200

    Replace vimwiki with wiki.vim

--- home/private_dot_config/nvim/lua/plugins/notes.lua
+++ home/private_dot_config/nvim/lua/plugins/notes.lua
@@ -1,22 +1,8 @@
 return {
     {
-        -- vimwiki https://github.com/vimwiki/vimwiki
-        "vimwiki/vimwiki",
-        init = function()
-            vim.g.vimwiki_diary_rel_path = "journals"
-            vim.g.vimwiki_folding = ""
-            vim.g.vimwiki_list = {
-                {
-                    path = "~/Notes/home/",
-                    synatx = "default",
-                },
-            }
-        end,
-    },
-
-    {
-        -- vim-zettel https://github.com/michal-h21/vim-zettel
-        "michal-h21/vim-zettel",
-        dependencies = { "junegunn/fzf", "junegunn/fzf.vim", },
+        "lervag/wiki.vim",
+        config = function()
+            vim.g.wiki_root = "~/Notes/coding/"
+        end
     },
 }
