2e31fc0af16d47a149876f6d47866925b3886751
Daniel Dirks <dandirks@web.de>
2023-08-30 22:02:00 +0200

    refactor(neovim): change notes directory

--- home/private_dot_config/nvim/lua/plugins/notes.lua
+++ home/private_dot_config/nvim/lua/plugins/notes.lua
@@ -2,7 +2,7 @@ return {
     {
         "lervag/wiki.vim",
         config = function()
-            vim.g.wiki_root = "~/Notes/home/"
+            vim.g.wiki_root = "~/Notes/"
             vim.g.wiki_write_on_nav = 1
         end
     },
