12cf9718ce411d807aaaa5891c321fd2366bc79e
Daniel Dirks <dandirks@web.de>
2023-10-21 19:25:06 +0200

    feat(neovim): add code outline

--- home/private_dot_config/nvim/lua/plugins/code.lua
+++ home/private_dot_config/nvim/lua/plugins/code.lua
@@ -169,4 +169,59 @@ return {
             vim.keymap.set("n", "<leader>cd", ":Copilot disable<cr>", { desc = "Disable Copilot" })
         end,
     },
+
+    {
+        -- symbols-outline.nvim https://github.com/simrat39/symbols-outline.nvim
+        "simrat39/symbols-outline.nvim",
+        init = function()
+            vim.keymap.set('n', '<leader>o', ':SymbolsOutline<cr>', { desc = 'Show outline' })
+        end,
+        opts = {
+            auto_close = true,
+            keymaps = {
+                close = {"<Esc>", "q"},
+                goto_location = "<Cr>",
+                focus_location = "o",
+                hover_symbol = "<C-space>",
+                toggle_preview = "K",
+                rename_symbol = "r",
+                code_actions = "a",
+                fold = "h",
+                unfold = "l",
+                fold_all = "W",
+                unfold_all = "E",
+                fold_reset = "R",
+            },
+            symbols = {
+                File = { icon = "", hl = "@text.uri" },
+                Module = { icon = "", hl = "@namespace" },
+                Namespace = { icon = "", hl = "@namespace" },
+                Package = { icon = "", hl = "@namespace" },
+                Class = { icon = "𝓒", hl = "@type" },
+                Method = { icon = "ƒ", hl = "@method" },
+                Property = { icon = "", hl = "@method" },
+                Field = { icon = "", hl = "@field" },
+                Constructor = { icon = "", hl = "@constructor" },
+                Enum = { icon = "ℰ", hl = "@type" },
+                Interface = { icon = "ﰮ", hl = "@type" },
+                Function = { icon = "", hl = "@function" },
+                Variable = { icon = "", hl = "@constant" },
+                Constant = { icon = "", hl = "@constant" },
+                String = { icon = "𝓐", hl = "@string" },
+                Number = { icon = "#", hl = "@number" },
+                Boolean = { icon = "⊨", hl = "@boolean" },
+                Array = { icon = "", hl = "@constant" },
+                Object = { icon = "⦿", hl = "@type" },
+                Key = { icon = "🔐", hl = "@type" },
+                Null = { icon = "NULL", hl = "@type" },
+                EnumMember = { icon = "", hl = "@field" },
+                Struct = { icon = "𝓢", hl = "@type" },
+                Event = { icon = "🗲", hl = "@type" },
+                Operator = { icon = "+", hl = "@operator" },
+                TypeParameter = { icon = "𝙏", hl = "@parameter" },
+                Component = { icon = "", hl = "@function" },
+                Fragment = { icon = "", hl = "@constant" },
+            },
+        },
+    }
 }
