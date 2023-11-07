2a4bc6dd8dbd0b82388bd2e9e84cec9275a1712c
Daniel Dirks <dandirks@web.de>
2023-11-07 16:17:16 +0100

    feat(neovim): add keymaps

--- home/private_dot_config/nvim/lua/core/mappings.lua
+++ home/private_dot_config/nvim/lua/core/mappings.lua
@@ -1,2 +1,11 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ";"
+
+-- buffers
+vim.keymap.set("n", "<leader>bn", ":bn<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bq", ":bd<cr>", { desc = "Close buffer" })
+vim.keymap.set("n", "<leader>ba", ":%bd<cr>", { desc = "Close all buffers" })
+vim.keymap.set("n", "<leader>bo", ":%bd|e#<cr>", { desc = "Close other buffers" })
+
+-- replace
+vim.keymap.set("n", "<leader>rn", ":%s/\\r//g<cr>", { desc = "Remove carriage return" })
