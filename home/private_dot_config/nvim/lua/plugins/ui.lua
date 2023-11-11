2495adbb6ae1d26d1c3d1e43c9ab174eb6cda3fe
Daniel Dirks <dandirks@web.de>
2023-11-12 00:12:13 +0100

    feat(neovim): show marks in gutter line

--- home/private_dot_config/nvim/lua/plugins/ui.lua
+++ home/private_dot_config/nvim/lua/plugins/ui.lua
@@ -259,4 +259,16 @@ return {
             scope = { show_start = false },
         },
     },
+
+    {
+        -- marks.nvim https://github.com/chentoast/marks.nvim
+        "chentoast/marks.nvim",
+        init = function()
+            vim.api.nvim_create_autocmd({ "BufRead" }, { command = ":delm a-zA-Z0-9", })  -- remove all marks on read
+
+            local mocha = require("catppuccin.palettes").get_palette "mocha"
+            vim.api.nvim_command('highlight MarkSignHL term=NONE cterm=NONE ctermbg=NONE ctermfg=red gui=NONE guibg=NONE guifg='..mocha.peach)
+        end,
+        opts = {}
+    }
 }
