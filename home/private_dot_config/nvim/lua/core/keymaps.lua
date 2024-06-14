effd1e970e2d4cd86dcaa41915dcd9ca6a0f7ca9
Daniel Dirks <dandirks@web.de>
2024-06-14 10:47:43 +0200

    feat(neovim): add keymap for previous buffer

--- home/private_dot_config/nvim/lua/core/keymaps.lua
+++ home/private_dot_config/nvim/lua/core/keymaps.lua
@@ -3,6 +3,7 @@ vim.g.maplocalleader = ";"
 
 -- buffers
 vim.keymap.set("n", "<leader>bn", ":bn<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bp", ":bp<cr>", { desc = "Previous buffer" })
 vim.keymap.set("n", "<leader>bq", ":bd<cr>", { desc = "Close buffer" })
 vim.keymap.set("n", "<leader>ba", ":%bd<cr>", { desc = "Close all buffers" })
 vim.keymap.set("n", "<leader>bo", ":%bd|e#<cr>", { desc = "Close other buffers" })
