3693b0c572a1367d4a5d01572b93d1631362947f
Daniel Dirks <dandirks@web.de>
2024-07-14 18:19:11 +0200

    fix: update nvim-java plugin config

--- home/private_dot_config/nvim/lua/plugins/code.lua
+++ home/private_dot_config/nvim/lua/plugins/code.lua
@@ -121,19 +121,7 @@ return {
         -- nvim-java https://github.com/nvim-java/nvim-java
         "nvim-java/nvim-java",
         ft = { "java" },
-        dependencies = {
-            "nvim-java/lua-async-await",
-            "nvim-java/nvim-java-refactor",
-            "nvim-java/nvim-java-core",
-            "nvim-java/nvim-java-test",
-            "nvim-java/nvim-java-dap",
-            "MunifTanjim/nui.nvim",
-            "neovim/nvim-lspconfig",
-            "mfussenegger/nvim-dap",
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-        },
-        opts = {},
+        config = false,
     },
 
     -- syntax
