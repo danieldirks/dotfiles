9e7c6d8259be5ab079751bb9f4e62a12d60be4c3
Daniel Dirks <dandirks@web.de>
2024-07-23 23:42:52 +0200

    feat(i3): add config

---
+++ home/private_dot_config/i3/executable_workspaces.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# adds inactive default workspaces to i3 workspace area
+i3-msg -t subscribe -m '["workspace"]' | {
+    i3-msg -t get_workspaces;
+    while read EVENT; do i3-msg -t get_workspaces; done;
+} | jq --unbuffered -c '
+    def fake_ws(num; name): {
+        num: num,
+        name: name,
+        visible: false,
+        focused: false,
+        urgent: false
+    };
+    . + [ fake_ws(1; "1:"), fake_ws(2; "2:"), fake_ws(3; "3:"), fake_ws(4; "4:") ] | unique_by(.name) '
