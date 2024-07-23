a452a8c8cb68423230817663db6736e7083e8d83
Daniel Dirks <dandirks@web.de>
2024-07-23 19:31:05 +0200

    feat(i3): add locking script

---
+++ home/private_dot_config/i3/executable_lock.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sleep .1
+rm -f "$HOME/.cache/lock.png"
+
+# make screenshot
+scrot -z "$HOME/.cache/lock.png"
+
+# blur and dim screenshot
+magick "$HOME/.cache/lock.png" -scale 5% -scale 2000% -fill black -colorize 20% "$HOME/.cache/lock.png"
+
+# add symbol to screenshot
+#GEOMETRY=""
+#if [ $((`DISPLAY=:0 xrandr -q | grep ' connected' | wc -l` % 2)) -eq 0 ]; then
+#    GEOMETRY="-geometry +960"
+#fi
+#magick "$HOME/.cache/lock.png" "$HOME/.config/i3/lock.png" $GEOMETRY -gravity center -composite "$HOME/.cache/lock.png"
+
+# lock using i3lock
+i3lock --no-unlock-indicator --nofork --image="$HOME/.cache/lock.png"
