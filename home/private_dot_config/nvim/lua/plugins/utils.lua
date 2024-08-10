e54005d359a2557c7297f6a4b388d064493dc374
Daniel Dirks <dandirks@web.de>
2024-08-10 15:35:50 +0200

    feat(neovim,tmux): add image rendering support

--- home/private_dot_config/nvim/lua/plugins/utils.lua
+++ home/private_dot_config/nvim/lua/plugins/utils.lua
@@ -162,4 +162,38 @@ return {
             "CodeSnapHighlightSave",
         },
     },
+
+    {
+        -- image.nvim https://github.com/3rd/image.nvim
+        -- support for inline images using terminal image protocol
+        "3rd/image.nvim",
+        build = "luarocks install --local --lua-version 5.1 magick",
+        opts = {
+            backend = "kitty",
+            integrations = {
+                markdown = {
+                    enabled = true,
+                    clear_in_insert_mode = true,
+                    download_remote_images = true,
+                    only_render_image_at_cursor = false,
+                    filetypes = { "markdown", "vimwiki" },
+                },
+                html = {
+                    enabled = false,
+                },
+                css = {
+                    enabled = false,
+                },
+            },
+            max_width = nil,
+            max_height = nil,
+            max_width_window_percentage = nil,
+            max_height_window_percentage = 50,
+            window_overlap_clear_enabled = false,
+            window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+            editor_only_render_when_focused = false,
+            tmux_show_only_in_active_window = false,
+            hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
+        },
+    },
 }
