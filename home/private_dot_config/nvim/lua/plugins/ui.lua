20e451ef08659e9a3d8aa439a091c9f37c559f0e
Daniel Dirks <dandirks@web.de>
2024-09-06 16:07:13 +0200

    feat(neovim): add oil.nvim default options

--- home/private_dot_config/nvim/lua/plugins/ui.lua
+++ home/private_dot_config/nvim/lua/plugins/ui.lua
@@ -314,15 +314,69 @@ return {
             vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
         end,
         opts = {
+            columns = {
+                "icon",
+                -- "permissions",
+                -- "size",
+                -- "mtime",
+            },
+            constrain_cursor = "editable",  -- editable, name, false
             default_file_explorer = true,
             delete_to_trash = true,
-            skip_confirm_for_simple_edits = true,
+            git = {
+                add = function(path)
+                    return true
+                end,
+                mv = function(src_path, dest_path)
+                    return true
+                end,
+                rm = function(path)
+                    return true
+                end,
+            },
             keymaps = {
+                ["g?"] = "actions.show_help",
+                ["<CR>"] = "actions.select",
+                ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+                ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+                ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+                ["<C-p>"] = "actions.preview",
+                ["<C-c>"] = "actions.close",
+                ["<C-l>"] = "actions.refresh",
                 ["<bs>"] = "actions.parent",
+                ["-"] = "actions.parent",
+                ["_"] = "actions.open_cwd",
+                ["`"] = "actions.cd",
+                ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+                ["gs"] = "actions.change_sort",
+                ["gx"] = "actions.open_external",
+                ["g."] = "actions.toggle_hidden",
+                ["g\\"] = "actions.toggle_trash",
             },
+            preview = {
+                max_width = { 100, 0.8 },
+                min_width = { 40, 0.4 },
+                width = nil,
+                max_height = 0.9,
+                min_height = { 5, 0.1 },
+                height = nil,
+                border = "rounded",
+                win_options = {
+                    winblend = 0,
+                },
+                update_on_cursor_moved = true,
+            },
+            skip_confirm_for_simple_edits = true,
             view_options = {
-                show_hidden = true,
+                case_insensitive = false,
+                natural_order = true,
+                show_hidden = false,
+                sort = {
+                    { "type", "asc" },  -- folders before files
+                    { "name", "asc" },
+                },
             },
+            watch_for_changes = false,
         },
     }
 }
