ba728dd58789d7c879765a40c69de28f2c431801
Daniel Dirks <dandirks@web.de>
2024-11-01 22:21:17 +0100

    feat(neovim): reactivate vimwiki title autocomplete

--- home/private_dot_config/nvim/lua/plugins/completion.lua
+++ home/private_dot_config/nvim/lua/plugins/completion.lua
@@ -31,20 +31,20 @@ local function vimwiki_source()
     source._candidates = function(_, files, current_dir, callback)
         local items = {}
         for _, file in ipairs(files) do
+            -- Does not return relative paths for parent directories
+            -- See https://github.com/nvim-lua/plenary.nvim/issues/411
             local relative_path = Path:new(file):make_relative(current_dir)
 
-            -- use relative path without extension as name
+            -- use file name without extension as name
             local note_name = vim.fn.fnamemodify(relative_path, ":r")
-            local note_title = note_name
 
             -- determin page title from markdown
-            -- TODO: read async
-            --local note_title = Path:new(file):head(1)
-            --if note_title:match('^# .*') then
-            --    note_title = string.sub(note_title, 3)
-            --else
-            --    note_title = note_name
-            --end
+            local note_title = Path:new(file):head(1)
+            if note_title:match('^# .*') then
+                note_title = string.sub(note_title, 3)
+            else
+                note_title = note_name
+            end
 
             table.insert(items, {
                 label = note_name,
