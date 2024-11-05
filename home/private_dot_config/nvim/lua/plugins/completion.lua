736dcd60a7d9f4aa49e04c5dbe4fc5002707c8fb
Daniel Dirks <dandirks@web.de>
2024-11-05 19:00:18 +0100

    feat(neovim): add super tab for snippet autocompletion

--- home/private_dot_config/nvim/lua/plugins/completion.lua
+++ home/private_dot_config/nvim/lua/plugins/completion.lua
@@ -74,6 +74,7 @@ return {
             "saadparwaiz1/cmp_luasnip",
         },
         config = function()
+            local luasnip = require("luasnip")
             local cmp = require("cmp")
             vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
@@ -88,7 +89,39 @@ return {
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ['<CR>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            if luasnip.expandable() then
+                                luasnip.expand()
+                            else
+                                cmp.confirm({
+                                    select = true,
+                                })
+                            end
+                        else
+                            fallback()
+                        end
+                    end),
+
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.locally_jumpable(1) then
+                            luasnip.jump(1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
                 }),
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
