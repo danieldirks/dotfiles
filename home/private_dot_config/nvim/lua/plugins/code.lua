668eab936b14377a30dba27511811e19fb400218
Daniel Dirks <dandirks@web.de>
2024-11-20 11:41:06 +0100

    feat(neovim): disable treesitter on large files

--- home/private_dot_config/nvim/lua/plugins/code.lua
+++ home/private_dot_config/nvim/lua/plugins/code.lua
@@ -130,7 +130,15 @@ return {
         build = ":TSUpdate",
         opts = {
             auto_install = true,
-            highlight = { enable = true },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+                use_languagetree = false,
+                disable = function(_, bufnr)
+                    local file_size = vim.fn.getfsize(vim.fn.bufname(bufnr))
+                    return file_size > 5000
+                end,
+            },
         },
         config = function(_, opts)
             require("nvim-treesitter.configs").setup(opts)
