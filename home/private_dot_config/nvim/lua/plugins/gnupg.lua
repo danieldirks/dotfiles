4227f6f8e2bf7827b8753a3f57b75ba34a4137a2
Daniel Dirks <dandirks@web.de>
2025-04-24 10:57:26 +0200

    feat(neovim): add gpg encryption of *.md files to support vimwiki pages

--- home/private_dot_config/nvim/lua/plugins/gnupg.lua
+++ home/private_dot_config/nvim/lua/plugins/gnupg.lua
@@ -3,5 +3,10 @@ return {
         -- en/decrypt gnupg files automatically
         -- https://github.com/jamessan/vim-gnupg
         "jamessan/vim-gnupg",
+        lazy = false,
+        init = function ()
+            -- extend to support encryption of vimwiki pages that get .md appended implicitly
+            vim.g.GPGFilePattern = "*.{gpg,gpg.md,asc,asc.md,pgp,pgp.md}"
+        end
     },
 }
